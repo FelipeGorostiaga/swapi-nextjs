ffc8d1e22b3bfaaa92b3fb573c2abcee88be0e8c
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2025-01-23 01:09:53 -0300

    fix: text align

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="max-w-4xl w-full mx-auto gap-6 px-6 flex flex-col items-center justify-start">
-      <h2 className="mt-10 scroll-m-20 border-b pb-2 text-5xl font-semibold tracking-tight transition-colors">
+    <div className="max-w-4xl w-full mx-auto gap-6 px-6 flex flex-col items-center justify-start text-center">
+      <h2 className="mt-10 scroll-m-20 pb-2 text-5xl font-semibold tracking-tight transition-colors">
         Felipe Gorostiaga
       </h2>
-      <p className="text-xl text-muted-foreground text-neutral-300 text-center max-w-2xl">
+      <p className="text-xl text-muted-foreground text-neutral-300 max-w-2xl">
         Greetings, young Padawans. Present myself, I must! Skilled in the ways
         of coding, I am. From a galaxy far, far away, I come to show you my
         passion for web development. Strong, the Force in me is.
